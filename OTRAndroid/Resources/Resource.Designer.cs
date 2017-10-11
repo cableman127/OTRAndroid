ecfaadd829d054d3e7b3f8ee20b158e206b229df
cableman127 <trile@letech.com>
2017-10-11 16:01:09 -0400

    New

--- OTRAndroid/Resources/Resource.Designer.cs
+++ OTRAndroid/Resources/Resource.Designer.cs
@@ -28,6 +28,22 @@ namespace OTRAndroid
 		{
 		}
 		
+		public partial class Array
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int androidcolors = 2131099648;
+			
+			static Array()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Array()
+			{
+			}
+		}
+		
 		public partial class Attribute
 		{
 			
@@ -41,11 +57,66 @@ namespace OTRAndroid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int black = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int blue = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int cyan = 2131034114;
+			
+			// aapt resource value: 0x7f050008
+			public const int darkblue = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int darkgreen = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int darkorange = 2131034123;
+			
+			// aapt resource value: 0x7f050009
+			public const int darkpurple = 2131034121;
+			
+			// aapt resource value: 0x7f05000c
+			public const int darkred = 2131034124;
+			
+			// aapt resource value: 0x7f050005
+			public const int green = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int orange = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int purple = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int red = 2131034119;
+			
+			// aapt resource value: 0x7f050001
+			public const int yellow = 2131034113;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int custom_scroll_style = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -57,11 +128,72 @@ namespace OTRAndroid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f080000
+			public const int a1b = 2131230720;
+			
+			// aapt resource value: 0x7f080001
+			public const int a1t = 2131230721;
+			
+			// aapt resource value: 0x7f080002
+			public const int a2b = 2131230722;
+			
+			// aapt resource value: 0x7f080003
+			public const int a2t = 2131230723;
+			
+			// aapt resource value: 0x7f080004
+			public const int a3b = 2131230724;
+			
+			// aapt resource value: 0x7f080005
+			public const int a3t = 2131230725;
+			
+			// aapt resource value: 0x7f08000c
+			public const int a4b = 2131230732;
+			
+			// aapt resource value: 0x7f08000d
+			public const int a4t = 2131230733;
+			
+			// aapt resource value: 0x7f080006
+			public const int b1b = 2131230726;
+			
+			// aapt resource value: 0x7f080007
+			public const int b1t = 2131230727;
+			
+			// aapt resource value: 0x7f080008
+			public const int b2b = 2131230728;
+			
+			// aapt resource value: 0x7f080009
+			public const int b2t = 2131230729;
+			
+			// aapt resource value: 0x7f08000a
+			public const int b3b = 2131230730;
+			
+			// aapt resource value: 0x7f08000b
+			public const int b3t = 2131230731;
+			
+			// aapt resource value: 0x7f08000e
+			public const int b4b = 2131230734;
+			
+			// aapt resource value: 0x7f08000f
+			public const int b4t = 2131230735;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
 			static Layout()
 			{
@@ -73,14 +205,51 @@ namespace OTRAndroid
 			}
 		}
 		
+		public partial class Mipmap
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
+			
+			static Mipmap()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Mipmap()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f070001
+			public const int a1 = 2131165185;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f070002
+			public const int a2 = 2131165186;
+			
+			// aapt resource value: 0x7f070003
+			public const int a3 = 2131165187;
+			
+			// aapt resource value: 0x7f070007
+			public const int a4 = 2131165191;
+			
+			// aapt resource value: 0x7f070000
+			public const int app_name = 2131165184;
+			
+			// aapt resource value: 0x7f070004
+			public const int b1 = 2131165188;
+			
+			// aapt resource value: 0x7f070005
+			public const int b2 = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int b3 = 2131165190;
+			
+			// aapt resource value: 0x7f070008
+			public const int b4 = 2131165192;
 			
 			static String()
 			{
